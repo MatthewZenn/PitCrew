0142a89e6853c4797cb40e94a0afe8751bb60bd7
Matthew Zenn <spyguy57@gmail.com>
2021-06-27 13:11:06 -0400

    generated files

---
+++ main.js
@@ -0,0 +1,19 @@
+const electron = require('electron');
+const url = require('url');
+const path = require('path');
+
+const {app, BrowserWindow, Menu} = electron;
+Menu.setApplicationMenu(null);
+
+let mainWindow;
+
+app.on('ready', function(){
+    var screenElectron = electron.screen;
+    var mainScreen = screenElectron.getPrimaryDisplay();
+    var dimensions = mainScreen.size;
+    mainWindow = new BrowserWindow({ width: dimensions.width, height: dimensions.height, resizable: true, frame: false, icon: "Static/Logo.ico", webPreferences: {nodeIntegration: true}});
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'mainWindow.html'),
+        protocol: 'file:',
+        slashes: true
+    }))});
